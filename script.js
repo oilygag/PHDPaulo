489f43e3f1f2909dce9baf9be1c1599755024a2f
Alisa Gordeeva <alisagord617@gmail.com>
2023-03-31 12:44:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
 const playButton = document.getElementById('playButton');
-const soundEffect = new Audio('https://www.myinstants.com/media/sounds/we-are-the-champions.mp3');
-const fireworksLeft = document.getElementById('fireworksLeft');
-const fireworksRight = document.getElementById('fireworksRight');
-
-function createFirework() {
-  console.log('createFirework');
-  const firework = document.createElement('div');
-  firework.classList.add('firework');
-  const particle = document.createElement('span');
-  particle.classList.add('particle');
-  firework.appendChild(particle);
-  return firework;
-}
-
-function launchFirework(position) {
-  console.log('launchFirework');
-  const firework = createFirework();
-  firework.style.left = `${position}%`;
-  if (position === 0) {
-    fireworksLeft.appendChild(firework);
-  } else {
-    fireworksRight.appendChild(firework);
-  }
-  setTimeout(() => {
-    firework.remove();
-  }, 3000);
-}
+const soundEffect = document.createElement('audio');
+soundEffect.src = 'https://www.myinstants.com/media/sounds/we-are-the-champions.mp3';
 
 playButton.addEventListener('click', () => {
-  console.log('button clicked');
-  soundEffect.play();
-  launchFirework(0);
-  launchFirework(100);
+	soundEffect.play();
 });
